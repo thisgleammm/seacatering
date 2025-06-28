9b8691c7dac6a10a922e70b403077e400e3b63aa
thisgleammm <thisgleam@gmail.com>
2025-06-28 17:41:44 +0700

    style: update tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
-import {heroui} from "@heroui/theme"
+import { heroui } from "@heroui/theme";
 
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -16,10 +17,19 @@ const config = {
       colors: {
         primary: "#8C0909",
       },
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: "0", transform: "translateY(10px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 0.3s ease-in-out",
+      },
     },
   },
   darkMode: "class",
   plugins: [heroui()],
-}
+};
 
 module.exports = config;
